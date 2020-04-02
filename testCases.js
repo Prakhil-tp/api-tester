210f48e8335ec9231257248a9bcc14a38d248bb5
prakhil-tp <prakhil.tp@gmail.com>
2020-04-02 05:41:20 +0530

    Add test cases

--- testCases.js
+++ testCases.js
@@ -1,8 +1,45 @@
-const synchronousRequsts = async () => {
+const https = require('https');
+const axios = require('axios');
+const helpers = require('./helpers') 
 
+const {
+  TEST_API,
+  REQUEST_LIMIT
+} = process.env
+
+const synchronousRequsts = async () => {
+ for(let req=1; req <= REQUEST_LIMIT; req++) {
+   let startTime = new Date().getTime();
+   await axios.get(TEST_API);
+   let endTime = new Date().getTime();
+
+   helpers.appendToFile({
+    'TESTCASE': 'Test case 1',
+    'REQUEST': `Request ${req}`,
+    'START TIME': startTime,
+    'END TIME': endTime,
+    'ELAPSED TIME': endTime-startTime,
+    'API': TEST_API
+   })
+ }
 }
 const asynchrounsRequsts = async () => {
-  
+  function handleRequestResult () {
+    let endTime = new Date().getTime();
+    helpers.appendToFile({
+      'TESTCASE': 'Test case 2',
+      'REQUEST': `Request ${this.req}`,
+      'START TIME': this.startTime,
+      'END TIME': endTime,
+      'ELAPSED TIME': endTime - this.startTime ,
+      'API': this.api
+     })
+  }
+
+  for(let req=1; req <= REQUEST_LIMIT; req++) {
+    let startTime = new Date().getTime();
+    https.get(TEST_API, handleRequestResult.bind({startTime,api:TEST_API, req}));
+  }
 }
 
 module.exports = {
