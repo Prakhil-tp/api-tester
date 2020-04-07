a2341f1a938e2f1615e756ab96d9efbd86f4605f
prakhil-tp <prakhil.tp@gmail.com>
2020-04-07 12:53:48 +0530

    Automate script calling.

---
+++ automate.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+echo "Automation script starts"
+end=$((SECONDS+3600));
+counter=1
+while [ $SECONDS -lt $end ];
+do
+  npm start
+  mkdir -p output/
+  cp ./results.csv "./output/resutls_${counter}.csv" 
+  counter=$((counter+=1))
+done
+cp ./avg.csv ./output/avg.csv
+echo "script ends."
